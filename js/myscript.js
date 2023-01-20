59f43810dd74ef96323a10dd642029202b06231b
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-01-20 15:02:26 +0100

    insert name and surname

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+//CHIEDO IL NOME ALL'UTENTE
+let firstName = prompt("Qual'è il tuo nome?");
+console.log(firstName);
+
+//CHIEDO IL COGNOME ALL'UTENTE
+let surName = prompt("Qual'è il tuo cognome?");
+console.log(surName);
+
+//CONCATENO NOME E COGNOME
+let welcome_msg = `Benvenuto ${firstName} ${surName}`;
+console.log(welcome_msg);
