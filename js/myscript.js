7b06deaa11c95156fb934077321b9396d23d5b96
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-01-20 15:25:25 +0100

    add let int

--- js/myscript.js
+++ js/myscript.js
@@ -1,20 +1,30 @@
 //CHIEDO IL NOME ALL'UTENTE
 let firstName = prompt("Qual'è il tuo nome?");
+
 console.log(firstName);
+
 
 //CHIEDO IL COGNOME ALL'UTENTE
 let surName = prompt("Qual'è il tuo cognome?");
+
 console.log(surName);
+
 
 //CHIEDO ALL'UTENTE IL SUO COLORE PREFERITO
 let favouriteColour = prompt("Qual'è il tuo colore preferito?");
+
 console.log(favouriteColour);
 
+
+//RENDO IL NUMERO 23 UNA VARIABILE INT INVECE DI STRING
+let myInt = 23;
+
+
 //CONCATENO NOME E COGNOME E COLORE PREFERITO
-let welcome_msg = `Benvenuto 
-${firstName} ${surName}, 
-il tuo colore preferito è il ${favouriteColour}`;
+let welcome_msg = `${firstName}${surName}${favouriteColour}${myInt}`;
+
 console.log(welcome_msg);
+
 
 //SCRIVO NELL'HTML ALL'INTERNO DEL TAG h1
 let my_welcome_title = document.getElementById("welcome_title");
